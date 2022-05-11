ae592d9f1a181a55cce6370c451fe3c2b0315975
betoann <toandgkthd12@gmail.com>
2022-05-11 15:03:52 +0700

    update code data

--- index.js
+++ index.js
@@ -23,59 +23,223 @@ app.set("view engine", "ejs");
 // cấp quyền truy cập cho thư mục public
 app.use(express.static("public"));
 
+
 app.get("/",function (req,res){
-    res.render("home");
+    const sql_txt = "select * from products";
+    conn.query(sql_txt,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("home", {
+                data_prod_home: result
+            })
+        }
+        else res.send("404 Not found");
+    });
 })
+
+
 
 app.get("/products",function (req,res){
     const sql_txt = "select * from products";
     conn.query(sql_txt,function (err,result){
-        if (err) res.render(err.message)
+        if (err) res.send(err.message)
         else if (result.length > 0){
             res.render("products", {
+                title:"Products",
                 products: result
             })
         }
-    else res.send("404 Not found");
+        else res.send("404 Not found");
     });
 })
 
 app.get("/ladies-glasses",function (req,res){
-    const sql_txt = "select * from products where UserObject like 'All' or UserObject like 'Women'";
+    const sql_txt = "select * from products where UserObject = 'All' or UserObject = 'Women'";
     conn.query(sql_txt,function (err,result){
-        if (err) res.render(err.message)
+        if (err) res.send(err.message)
         else if (result.length > 0){
-            res.render("display_glasses", {
-                title_products : "Ladies glasses",
-                display_products: result
+            res.render("ladies_glasses", {
+                data_ladies: result
             })
         }
         else res.send("404 Not found");
     });
 })
+app.get("/ladies-glasses/sort-name",function (req,res){
+    const name = req.query.name || "";
+    const sql_sort_name = "select * from products where UserObject = 'All' or UserObject = 'Women' order by ProductName "+name;
+    conn.query(sql_sort_name,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("ladies_glasses", {
+                data_ladies: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
+app.get("/ladies-glasses/sort-price",function (req,res){
+    const price = req.query.price || "";
+    const sql_price = "select * from products where UserObject = 'All' or UserObject = 'Women' order by PriceSale "+price;
+    conn.query(sql_price,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("ladies_glasses", {
+                data_ladies: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
+
+
 app.get("/men-glasses",function (req,res){
-    const sql_txt = "select * from products where UserObject like 'All' or UserObject like 'Men'";
+    const sql_txt = "select * from products where UserObject = 'All' or UserObject = 'Men'";
     conn.query(sql_txt,function (err,result){
-        if (err) res.render(err.message)
+        if (err) res.send(err.message)
         else if (result.length > 0){
-            res.render("display_glasses", {
-                title_products : "Men's glasses",
-                display_products: result
+            res.render("men_glasses", {
+                data_men: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
+app.get("/men-glasses/sort-name",function (req,res){
+    const name = req.query.name || "";
+    const sql_sort_name = "select * from products where UserObject = 'All' or UserObject = 'Men' order by ProductName "+name;
+    conn.query(sql_sort_name,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("men_glasses", {
+                data_men: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
+app.get("/men-glasses/sort-price",function (req,res){
+    const price = req.query.price || "";
+    const sql_price = "select * from products where UserObject = 'All' or UserObject = 'Men' order by PriceSale "+price;
+    conn.query(sql_price,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("men_glasses", {
+                data_men: result
             })
         }
         else res.send("404 Not found");
     });
 })
 
+app.get("/glass-frames",function (req,res){
+    const sql_txt = "select * from products where Type = 'Frame'";
+    conn.query(sql_txt,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("glasses_frame", {
+                data_frame: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
+app.get("/glass-frames/sort-name",function (req,res){
+    const name = req.query.name || "";
+    const sql_sort_name = "select * from products where Type = 'Frame' order by ProductName "+name;
+    conn.query(sql_sort_name,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("glasses_frame", {
+                data_frame: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
+app.get("/glass-frames/sort-price",function (req,res){
+    const price = req.query.price || "";
+    const sql_price = "select * from products where Type = 'Frame' order by PriceSale "+price;
+    conn.query(sql_price,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("glasses_frame", {
+                data_frame: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
 
+app.get("/brand",function (req,res){
+    const brand = req.query.brand || "";
+    const sql_txt = "select * from products left join brand on products.BrandName = brand.BrandName " +
+        "where products.BrandName like '%"+brand+"%'";
+    conn.query(sql_txt,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("display_brand", {
+                data_brand_products: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
 
 app.get("/products/detail-product",function (req,res){
-    res.render("detail_product");
+    const id = req.query.id || "";
+    const sql_txt = "select * from products where ID = "+id;
+    conn.query(sql_txt,function (err,result) {
+        if(err) res.send(err.message);
+        else if(result.length > 0)
+            res.render("detail_product",{
+                product:result[0]
+            });
+        else res.send("404 Not found");
+    })
 })
 
-app.get("/shopping-cart",function (req,res){
-    res.render("shopping_cart");
+app.get("/glasses",function (req,res){
+    const sql_txt = "select * from products where Type = 'Glasses'";
+    conn.query(sql_txt,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("products", {
+                title:"Glasses",
+                products: result
+            })
+        }
+        else res.send("404 Not found");
+    });
 })
+
+app.get("/search-results",function (req,res){
+    const valuae = req.query.search;
+    const sql_txt = "select * from products where ProductName like "+"'%"+valuae+"%'";
+    conn.query(sql_txt,function (err,result){
+        if (err) res.send(err.message)
+        else if (result.length > 0){
+            res.render("search_results", {
+                data_search: result
+            })
+        }
+        else res.send("404 Not found");
+    });
+})
+
+
+app.get("/shopping-cart",function (req,res){
+    const sql_txt = "select * from shoppingcart";
+    conn.query(sql_txt,function (err,result){
+        if (err) res.send(err.message)
+        else{
+            res.render("shopping_cart", {
+                products_cart: result
+            })
+        }
+    });
+})
+
 
 app.get("/about-us",function (req,res){
     res.render("aboutus");
@@ -113,6 +277,7 @@ app.get("/wishlist",function (req,res){
 app.get("/terms-and-conditions",function (req,res){
     res.render("terms_and_conditions");
 })
+
 app.get("/security-notifications",function (req,res){
     res.render("security_notifications");
 })
